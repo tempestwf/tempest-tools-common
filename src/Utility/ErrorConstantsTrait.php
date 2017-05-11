667bdd1877d1f385c5c839e758b4a144fa5c1a26
Will Ferrer <wferrer@xfire.com>
2017-05-11 16:30:52 -0700

    localizing exceptions

--- src/Utility/ErrorConstantsTrait.php
+++ src/Utility/ErrorConstantsTrait.php
@@ -7,11 +7,11 @@ trait ErrorConstantsTrait
     public function getErrorFromConstant (string $errorName): array
     {
         if (static::ERRORS === null) {
-            throw new \RuntimeException('Error: ErrorConstantsTrait apply to a class with ERRORS constant');
+            throw new \RuntimeException(_('Error: ErrorConstantsTrait apply to a class with ERRORS constant'));
         }
 
         if (!isset(static::ERRORS[$errorName])) {
-            throw new \RuntimeException('Error: ErrorConstantsTrait was passed an error name that was not found in the ERRORS constant');
+            throw new \RuntimeException(_('Error: ErrorConstantsTrait was passed an error name that was not found in the ERRORS constant'));
         }
         return static::ERRORS[$errorName];
     }
