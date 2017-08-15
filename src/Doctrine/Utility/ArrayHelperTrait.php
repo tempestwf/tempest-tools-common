809a30b88e41f35d84a659de0073e3701874f445
Will Ferrer <wferrer@xfire.com>
2017-08-15 16:24:00 -0700

    Getting rid of those annoying warnings in test case

---
+++ src/Doctrine/Utility/ArrayHelperTrait.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace TempestTools\Common\Helper;
+
+use \TempestTools\Common\Contracts\ArrayHelperContract;
+
+
+trait ArrayHelperTrait
+{
+    /**
+     * @var ArrayHelper|null $arrayHelper
+     */
+    protected $arrayHelper;
+
+
+    /**
+     * Passes it's self to the extractor
+     *
+     * @return \ArrayObject
+     * @throws \RuntimeException
+     */
+    public function extractSelf (): \ArrayObject
+    {
+        /** @noinspection NullPointerExceptionInspection */
+        return $this->arrayHelper()->extract([$this]);
+    }
+
+    /**
+     * @param null|ArrayHelperContract $arrayHelper
+     */
+    public function setArrayHelper(ArrayHelperContract $arrayHelper): void
+    {
+        $this->arrayHelper = $arrayHelper;
+    }
+
+    /**
+     * @return null|ArrayHelperContract
+     */
+    public function getArrayHelper():?ArrayHelperContract
+    {
+        return $this->arrayHelper;
+    }
+
+    /**
+     * Gets existing array helper, or makes new one and then returns it
+     *
+     * @return null|ArrayHelperContract
+     */
+    public function arrayHelper():ArrayHelperContract {
+        $arrayHelper = $this->getArrayHelper();
+        if ($arrayHelper === NULL) {
+            $arrayHelper = new ArrayHelper();
+            $this->setArrayHelper($arrayHelper);
+        }
+        return $arrayHelper;
+    }
+
+}
