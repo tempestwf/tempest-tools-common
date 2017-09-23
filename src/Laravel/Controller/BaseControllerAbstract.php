e638e3016f5912b0f5e8c00936d864da6116a462
Will Ferrer <wferrer@xfire.com>
2017-09-22 18:39:29 -0700

    Moving ahead with Controller implementation

--- src/Laravel/Controller/BaseControllerAbstract.php
+++ src/Laravel/Controller/BaseControllerAbstract.php
@@ -6,9 +6,15 @@ namespace TempestTools\Common\Laravel\Controller;
 
 use Illuminate\Routing\Controller;
 use TempestTools\Common\Helper\ArrayHelperTrait;
+use Illuminate\Support\Facades\Event;
 
 abstract class BaseControllerAbstract extends Controller
 {
     use ArrayHelperTrait;
+
+    public function __construct()
+    {
+        Event::subscribe($this);
+    }
 
 }
